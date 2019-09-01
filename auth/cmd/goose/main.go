85ad425797fd08187e4f856170ebe7bbd8cf55c5
Bubunyo Nyavor <bubu@afradio.co>
2019-09-01 18:04:52 +0000

    Setup goose for migration

---
+++ auth/cmd/goose/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/bubunyo/para-services/db/migrations"
+	_ "github.com/lib/pq"
+	"github.com/pressly/goose"
+)
+
+var (
+	flags = flag.NewFlagSet("goose", flag.ExitOnError)
+	dir   = flags.String("dir", "./db/migrations", "directory with migration files")
+)
+
+func main() {
+	_ = flags.Parse(os.Args[1:])
+	args := flags.Args()
+
+	if len(args) < 1 {
+		flags.Usage()
+		return
+	}
+
+	var (
+		host     = os.Getenv("DB_HOST")
+		port     = 5432
+		dbname   = os.Getenv("DB_DATABASE")
+		user     = os.Getenv("DB_USER")
+		password = os.Getenv("DB_PASSWORD")
+	)
+
+	fmt.Println(host, dbname, user, password)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("goose: failed to open DB: %v\n", err)
+	}
+
+	var arguments []string
+	if len(args) > 1 {
+		arguments = append(arguments, args[0:]...)
+	}
+
+	command := args[0]
+
+	if err := goose.Run(command, db, *dir, arguments...); err != nil {
+		log.Fatalf("goose %v: %v", command, err)
+	}
+}
