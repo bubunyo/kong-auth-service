ed7ff713a31f6bcc9d543347510bd0097532b866
Bubunyo Nyavor <bubu@afradio.co>
2019-09-02 09:01:19 +0000

    Add auth build to Dockerfile, Fix some comments in auth

--- auth/main.go
+++ auth/main.go
@@ -14,7 +14,8 @@ import (
 
 func main() {
 	var wait time.Duration
-	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15,
+		"the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
 	flag.Parse()
 
 	r := mux.NewRouter()
@@ -22,14 +23,14 @@ func main() {
 
 	srv := &http.Server{
 		Addr: "0.0.0.0:8080",
-		// Good practice to set timeouts to avoid Slowloris attacks.
+		// Set timeouts to avoid Slowloris attacks.
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 		IdleTimeout:  time.Second * 60,
-		Handler:      r, // Pass our instance of gorilla/mux in.
+		Handler:      r,
 	}
 
-	// Run our server in a goroutine so that it doesn't block.
+	// Run server in a goroutine so that it doesn't block.
 	go func() {
 		if err := srv.ListenAndServe(); err != nil {
 			log.Println(err)
